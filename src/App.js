377e93179574959a70c4959021b90f93d31890ef
Sohaib Farooqi <sohaibfarooqi@hotmail.com>
2017-05-24 16:47:47 +0500

    Clock Component

--- src/App.js
+++ src/App.js
@@ -39,31 +39,6 @@ class Clock extends Component
 }
 export class App extends Component {
 
-  constructor(props)
-  {
-    super(props);
-    this.state = {date: new Date()};
-  }
-  
-  componentDidMount() 
-  {
-    this.timerID = setInterval(
-      () => this.tick(),
-      1000
-    );
-  }
-
-  componentWillUnmount()
-  {
-    clearInterval(this.timerID);
-  }
-
-  tick() {
-    this.setState({
-      date: new Date()
-    });
-  }
-
   render() {
     return (
       <div className="App">
