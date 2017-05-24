988aa9474c1b7e95820b890dc20d0ff2d9e4da69
Sohaib Farooqi <sohaibfarooqi@hotmail.com>
2017-05-24 16:37:25 +0500

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# APPP
+
+This project is bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
---
+++ src/App.js
@@ -0,0 +1,80 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class Clock extends Component
+{
+  constructor(props)
+  {
+    super(props);
+    this.state = {date: new Date()};
+  }
+  
+  componentDidMount() 
+  {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount()
+  {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render(){
+    return (
+      <div>
+      <h2>Time is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+export class App extends Component {
+
+  constructor(props)
+  {
+    super(props);
+    this.state = {date: new Date()};
+  }
+  
+  componentDidMount() 
+  {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount()
+  {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+        <Clock />
+        </p>
+      </div>
+    );
+  }
+}
